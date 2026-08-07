706f61eb2a6636f468bf550e9761bddcbbbce0fc
choua2025 <vchoua05@gmail.com>
2026-08-07 14:54:39 +0700

    t1

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 6000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, World! This is a simple Express server running on port 6000.");
+    res.send("Hello, World! This is a simple Express server running on port ");
 });
 
 
